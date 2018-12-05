6aca4ca94672f0b5514f489f403c120c7745789b
GilsonQueiroz <gilsontq@yahoo.com>
2018-12-04 23:35:04 -0300

    recuperar senha

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -4,10 +4,12 @@ namespace Hcode\Model;
 
 use Hcode\DB\SqL;
 use Hcode\Model;
+use Hcode\Mailer;
 
 class User extends Model{
 
 	const SESSION = "User";
+	const SECRET = "TestPMRPseletivo";
 
 	public static function login($login, $password)
 	{
@@ -156,6 +158,115 @@ class User extends Model{
 		));
 
 	}
+
+	public static function getForgot($email)
+	{
+
+		$sql = new SqL();
+
+		$results = $sql->select("
+			SELECT * 
+			FROM tb_users a
+			INNER JOIN tb_persons b USING(idperson)
+			WHERE a.desemail = :email;
+		", array(
+			"email"=>$email
+		));
+
+		if (count($results) === 0) 
+		{
+			throw new \Exception("Não foi possível recuperar a senha.");
+		} else
+		{
+			$data = $results[0];
+
+			$results2 = $sql->select("CALL sp_userpassrecoveries_create(:iduser, :desip)", array(
+				":iduser"=>$data["iduser"],
+				":desip"=>$_SERVER["REMOTE_ADDR"]
+			));
+
+			if (count($results2) === 0)
+			{
+				throw new \Exception("Não foi possível recuperar a senha.");
+			}
+			else
+			{
+				$dataRecovery = $results2[0];
+
+				$code = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, User::SECRET, $dataRecovery["idrecovery"], MCRYPT_MODE_ECB));
+
+				$link = "http://www.seletivo.rondondopara.pa.gov.br/admin/forgot/reset?code=$code";
+
+				$mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir Senha PMRP-Seletivo", "forgot", array(
+					"name"=>$data["desperson"],
+					"link"=>$link
+				));
+
+				$mailer->send();
+
+				return $data;
+
+			}
+		}
+
+	}
+
+	public static function validForgotDecrypt($code)
+	{
+
+		$idrecovery = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, User::SECRET, base64_decode($code), MCRYPT_MODE_ECB);
+
+		$sql = new SqL();
+
+		$results = $sql->select("
+			SELECT * FROM tb_userpassrecoveries a
+			INNER JOIN tb_users b USING(iduser)
+			INNER JOIN tb_persons c USING(idperson)
+			WHERE
+				a.idrecovery = :idrecovery
+			    AND
+			    a.dtrecovery IS NULL
+			    AND
+			    DATE_ADD(a.dtregister, INTERVAL 1 DAY) >= NOW();
+		", array(
+			":idrecovery"=>$idrecovery
+		));
+
+		if (count($results) === 0)
+		{
+			throw new \Exception("Não foi possível recuperar a senha");
+		}
+		else
+		{
+			return $results[0];
+		}
+
+	}
+
+
+	public static function setForgotUsed($idrecovery)
+	{
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_userpassrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", array(
+			":idrecovery"=>$idrecovery
+		));
+
+	}
+
+	public function setPassword($password)
+	{
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser", array(
+			":password"=>$password,
+			":iduser"=>$this->getiduser()
+		));
+
+	}
+
 
 }
 
