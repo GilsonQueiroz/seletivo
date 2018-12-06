2f5b7e5764e59fd31057c5584553d0283ad805cd
GilsonQueiroz <gilsontq@yahoo.com>
2018-12-06 11:38:35 -0300

    image

--- views-cache/index.2290275d5e8b1215e76a54b4404a9ad4.rtpl.php
+++ views-cache/index.2290275d5e8b1215e76a54b4404a9ad4.rtpl.php
@@ -1,1 +1,152 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><h1>Página em Construção</h1>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><div id="main-container">
+            
+<section id="main-slider" class="no-margin tpl">
+    <div class="carousel slide">
+        <ol class="carousel-indicators">
+            <li data-target="#main-slider" data-slide-to="0" class=""></li>
+            <li data-target="#main-slider" data-slide-to="1" class=""></li>
+            <li data-target="#main-slider" data-slide-to="2" class="active"></li>
+        </ol>
+        <div class="carousel-inner">
+
+            <div class="item" style="background-image: url(/res/site/images/bg_home_cadastro.jpg)">
+                <div class="container">
+                    <div class="row slide-margin">
+                        <div class="col-sm-6">
+                            <div class="carousel-content">
+                                <h1 class="animation animated-item-1">Você já se cadastrou ?</h1>
+                                <h2 class="animation animated-item-2">Só assim você poderá participar dos processos seletivos</h2>
+                                <a class="btn-slide animation animated-item-3" href="/register">Cadastre-se</a>
+                                                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div><!--/.item-->
+
+            <div class="item" style="background-image: url(/res/site/images/bg_home_cadastro.jpg)">
+                <div class="container">
+                    <div class="row slide-margin">
+                        <div class="col-sm-6">
+                            <div class="carousel-content">
+                                <h1 class="animation animated-item-1">Você já se cadastrou ?</h1>
+                                <h2 class="animation animated-item-2">Só assim você poderá participar dos processos seletivos</h2>
+                                <a class="btn-slide animation animated-item-3" href="/register">Cadastre-se</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div><!--/.item-->
+
+            <div class="item active" style="background-image: url(/res/site/images/bg_home_cadastro.jpg)">
+                <div class="container">
+                    <div class="row slide-margin">
+                        <div class="col-sm-6">
+                            <div class="carousel-content">
+                                <h1 class="animation animated-item-1">Você já se cadastrou ?</h1>
+                                <h2 class="animation animated-item-2">Só assim você poderá participar dos processos seletivos</h2>
+                                <a class="btn-slide animation animated-item-3" href="/register">Cadastre-se</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div><!--/.item-->
+
+        </div><!--/.carousel-inner-->
+    </div><!--/.carousel-->
+    <a class="prev hidden-xs" href="#main-slider" data-slide="prev">
+        <i class="fa fa-chevron-left"></i>
+    </a>
+    <a class="next hidden-xs" href="#main-slider" data-slide="next">
+        <i class="fa fa-chevron-right"></i>
+    </a>
+</section><section class="pricing-page tpl skill-wrap2">
+    <section class="pricing-page">
+        <div class="container">
+            <div class="center">  
+                <h2>Processos de Seleção Disponíveis</h2>
+                <p class="lead">Aqui você encontrará todas as informações necessárias dos processos de seleção que estão disponíveis e seus respectivos períodos de inscrição</p>
+            </div>  
+            <div class="pricing-area text-center">
+                                <div class="row">
+                    <h1 style="color:#09639b; padding-bottom: 55px;">No momento não há nenhum processo seletivo disponível.</h1>
+                    <img src="/images/people/successful-man-writing-his-agenda.jpg" alt="">
+                </div>
+                            </div><!--/pricing-area-->
+        </div><!--/container-->
+    </section><!--/pricing-page-->
+</section><section id="feature" class="tpl skill-wrap2">
+    <div class="container">
+        <div class="center wow fadeInDown" style="visibility: hidden; animation-name: none;">
+            <h2><a href="#" name="a1">Principais Características</a></h2>
+            <p class="lead">As principais características do Processo Seletivo Simplificado são:</p>
+        </div>
+
+        <div class="row">
+            <div class="features">
+                <div class="col-md-4 col-sm-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms" style="visibility: hidden; animation-duration: 1000ms; animation-delay: 600ms; animation-name: none;">
+                    <div class="feature-wrap">
+                        <i class="fa fa-cogs"></i>
+                        <h2>SIPROS</h2>
+                        <h3>Realizado através do Sistema Integrado de Processo Seletivo Simplificado</h3>
+                    </div>
+                </div><!--/.col-md-4-->
+
+                <div class="col-md-4 col-sm-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms" style="visibility: hidden; animation-duration: 1000ms; animation-delay: 600ms; animation-name: none;">
+                    <div class="feature-wrap">
+                        <i class="fa fa-thumbs-up"></i>
+                        <h2>Temporários</h2>
+                        <h3>Exclusivamente para contratação temporária para atender necessidade provisória de excepcional interesse público.</h3>
+                    </div>
+                </div><!--/.col-md-4-->
+
+                <div class="col-md-4 col-sm-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms" style="visibility: hidden; animation-duration: 1000ms; animation-delay: 600ms; animation-name: none;">
+                    <div class="feature-wrap">
+                        <i class="fa fa-graduation-cap"></i>
+                        <h2>Nível Superior</h2>
+                        <h3>Etapas de Análise Curricular e Entrevista, obrigatoriamente.</h3>
+                    </div>
+                </div><!--/.col-md-4-->
+                
+                <div class="col-md-4 col-sm-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms" style="visibility: hidden; animation-duration: 1000ms; animation-delay: 600ms; animation-name: none;">
+                    <div class="feature-wrap">
+                        <i class="fa fa-graduation-cap"></i>
+                        <h2>Nível Médio e Fundamental</h2>
+                        <h3>Etapas de Análise Curricular.</h3>
+                    </div>
+                </div><!--/.col-md-4-->
+            </div><!--/.services-->
+        </div><!--/.row-->    
+    </div><!--/.container-->
+</section><section id="conatcat-info" class="tpl skill-wrap2">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8">
+                <div class="media contact-info wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms" style="visibility: hidden; animation-duration: 1000ms; animation-delay: 600ms; animation-name: none;">
+                    <div class="pull-left">
+                        <img src="/images/MetroUI_Phone.png" alt="fone" style="width: 100px;">
+                    </div>
+                    <div class="media-body" style="padding-top: 24px;">
+                        <h2>Tem uma pergunta ou precisa de ajuda?</h2>
+                        <p>Entre em contato com o órgão organizador do Processo Seletivo Simplificado.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</div>
+
+<div class="item active" style="background-image: url(/res/site/images/bg_home_cadastro.jpg)">
+                <div class="container">
+                    <div class="row slide-margin">
+                        <div class="col-sm-6">
+                            <div class="carousel-content">
+                                <h1 class="animation animated-item-1">Você já se cadastrou ?</h1>
+                                <h2 class="animation animated-item-2">Só assim você poderá participar dos processos seletivos</h2>
+                                <a class="btn-slide animation animated-item-3" href="/register">Cadastre-se</a>
+                                                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+
+
+
